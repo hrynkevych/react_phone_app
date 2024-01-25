9306d7be4e7af306eae948814647b046367dd2d1
Misha Hrynkevych <grinjk1993@gmail.com>
2024-01-25 12:18:54 +0100

    add_gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,3 @@
-// import { createRoot } from 'react-dom/client';
-// import { Root } from './Root';
-
-// const container = document.getElementById('root') as HTMLDivElement;
-
-// createRoot(container).render(<Root />);
-
 import ReactDOM from 'react-dom';
 import { Root } from './Root';
 
